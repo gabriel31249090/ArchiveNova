06438c7963b5349eadac8557ca45504e78b756d9
gabriel31249090 <gabriel31249090@gmail.com>
2026-09-14 13:22:52 -0400

    Add public series collections shelves and taxonomy pages

---
+++ archive-nova-vercel/app/collections/[id]/page.tsx
@@ -0,0 +1,2 @@
+import { PublicListPage } from '@/components/library/public-list-page'
+export default async function Page({params}:{params:Promise<{id:string}>}){const {id}=await params;return <PublicListPage kind="collection" id={id}/>} 
